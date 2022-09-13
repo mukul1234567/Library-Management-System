7b1d8b99b5074ed279e829e86b538d6af3e9083a
mukul1234567 <mukulchaudhari01@gmail.com>
2022-09-13 17:26:46 +0530

    Designed routes for book and user

--- server/router.go
+++ server/router.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/mukul1234567/Library-Management-System/api"
+	"github.com/mukul1234567/Library-Management-System/book"
+	"github.com/mukul1234567/Library-Management-System/user"
 )
 
 const (
@@ -16,7 +18,28 @@ func initRouter(dep dependencies) (router *mux.Router) {
 
 	router = mux.NewRouter()
 	router.HandleFunc("/ping", pingHandler).Methods(http.MethodGet)
-	// router.HandleFunc("/users", user.List(dep.UserService)).Methods(http.MethodGet).Headers(versionHeader, v1)
+
+	//User
+
+	router.HandleFunc("/users", user.Create(dep.UserService)).Methods(http.MethodPost)
+	router.HandleFunc("/users", user.List(dep.UserService)).Methods(http.MethodGet)
+	router.HandleFunc("/users/{id}", user.FindByID(dep.UserService)).Methods(http.MethodGet)
+	router.HandleFunc("/users/{id}", user.DeleteByID(dep.UserService)).Methods(http.MethodDelete)
+	router.HandleFunc("/users", user.Update(dep.UserService)).Methods(http.MethodPut)
+
+	//Book
+
+	router.HandleFunc("/books", book.Create(dep.BookService)).Methods(http.MethodPost)
+	router.HandleFunc("/books", book.List(dep.BookService)).Methods(http.MethodGet)
+	router.HandleFunc("/books/{id}", book.FindByID(dep.BookService)).Methods(http.MethodGet)
+	router.HandleFunc("/books/{id}", book.DeleteByID(dep.BookService)).Methods(http.MethodDelete)
+	router.HandleFunc("/books", book.Update(dep.BookService)).Methods(http.MethodPut)
+
+	//Transaction
+
+	router.HandleFunc("/trans/{book_id}/{user_id}", book.Create(dep.BookService)).Methods(http.MethodPost)
+	router.HandleFunc("/trans", book.List(dep.BookService)).Methods(http.MethodGet)
+	router.HandleFunc("/trans/{book_id}/{user_id}", book.Update(dep.BookService)).Methods(http.MethodPut)
 
 	return
 }
