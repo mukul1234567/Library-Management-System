660a19cfeae60d956e5871ba3be5a7b1d05c057f
mukul1234567 <mukulchaudhari01@gmail.com>
2022-09-17 09:42:47 +0530

    Declared New Error variables

--- user/errors.go
+++ user/errors.go
@@ -3,8 +3,12 @@ package user
 import "errors"
 
 var (
-	errEmptyID   = errors.New("User ID must be present")
-	errEmptyName = errors.New("User name must be present")
-	errNoUsers   = errors.New("No Users present")
-	errNoUserId  = errors.New("User is not present")
+	errEmptyID      = errors.New("user ID must be present")
+	errEmptyName    = errors.New("please fill all the details")
+	errNoUsers      = errors.New("no Users present")
+	errNoUserId     = errors.New("user is not present")
+	errInvalidEmail = errors.New("invalid Email Entered")
+	errInvalidMobNo = errors.New("invalid Mobile Number Entered")
+	errInvalidAge   = errors.New("invalid Age Entered")
+	errInvalidRole  = errors.New("invalid Role Entered")
 )
