c059ef3400af17a7d3ef75afb4d1e74007f1e791
mukul1234567 <mukulchaudhari01@gmail.com>
2022-09-17 09:42:15 +0530

    Updated the validate function with few user validations fro create and update request

--- user/domain.go
+++ user/domain.go
@@ -1,19 +1,11 @@
 package user
 
-import "github.com/mukul1234567/Library-Management-System/db"
+import (
+	"fmt"
+	"strings"
 
-type updateRequest struct {
-	ID        string `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Gender    string `json:"gender"`
-	Age       int    `json:"age"`
-	Address   string `json:"address"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-	MobileNum string `json:"mob_no"`
-	Role      string `json:"role"`
-}
+	"github.com/mukul1234567/Library-Management-System/db"
+)
 
 type createRequest struct {
 	ID        string `json:"id"`
@@ -28,6 +20,21 @@ type createRequest struct {
 	Role      string `json:"role"`
 }
 
+type updateRequest struct {
+	ID        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Gender    string `json:"gender"`
+	Age       int    `json:"age"`
+	Address   string `json:"address"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	MobileNum string `json:"mob_no"`
+	Role      string `json:"role"`
+}
+
+// OldPassword  string `json:"old_password"`
+// NewPassword  string `json:"new_password"`
 type findByIDResponse struct {
 	User db.User `json:"user"`
 }
@@ -37,17 +44,28 @@ type listResponse struct {
 }
 
 func (cr createRequest) Validate() (err error) {
-	if cr.FirstName == "" {
+	if cr.FirstName == "" || cr.LastName == "" || cr.Gender == "" || cr.Address == "" || cr.Email == "" || cr.Password == "" || cr.MobileNum == "" || cr.Role == "" {
 		return errEmptyName
+	}
+	if (strings.ToLower(cr.Role) != "user") && (strings.ToLower(cr.Role) != "admin") && (strings.ToLower(cr.Role) != "superadmin") {
+		fmt.Println(strings.ToLower(cr.Role))
+		return errInvalidRole
+	}
+	if !isEmailValid(cr.Email) {
+		return errInvalidEmail
+	}
+	if len(cr.MobileNum) != 10 {
+		return errInvalidMobNo
+	}
+	if cr.Age < 0 {
+		return errInvalidAge
 	}
 	return
 }
 
-func (ur updateRequest) Validate() (err error) {
-	if ur.ID == "" {
-		return errEmptyID
-	}
-	if ur.FirstName == "" {
+func (cr updateRequest) Validate() (err error) {
+	// if cr.FirstName == "" || cr.LastName == "" || cr.Gender == "" || cr.Address == "" || cr.Email == "" || cr.OldPassword == "" ||cr.NewPassword == "" || cr.MobileNum == "" || cr.Role == "" {
+	if cr.FirstName == "" || cr.LastName == "" || cr.Gender == "" || cr.Address == "" || cr.Email == "" || cr.Password == "" || cr.MobileNum == "" || cr.Role == "" {
 		return errEmptyName
 	}
 	return
