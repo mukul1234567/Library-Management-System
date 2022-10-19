5d21b6940f8ed520daaed1cd500f43155ae15879
mukul1234567 <mukulchaudhari01@gmail.com>
2022-10-19 11:44:06 +0530

    Added error handling functionalitites

--- user/handler.go
+++ user/handler.go
@@ -12,6 +12,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/mukul1234567/Library-Management-System/api"
 	"github.com/mukul1234567/Library-Management-System/db"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type JWTClaim struct {
@@ -33,19 +34,24 @@ var values_role = map[string]int{"user": 2, "admin": 1, "superadmin": 0}
 // values_role["admin"] = 1
 // values_role["superadmin"] = 0
 var v db.User
+var count int = 0
 
 func Login(service Service) http.HandlerFunc {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		// fmt.Println("Hi")
 		// var j JWTClaim
 		var x Authentication
-		resp, err1 := service.list(req.Context())
-
+		// fmt.Println("Hiii")
+		resp, err1 := service.List(req.Context())
+		// fmt.Println("Hello")
 		if err1 == errNoUsers {
 			// api.Success(rw, http.StatusCreated, api.Response{Message: "Enter List"})
+			// fmt.Println("Hello1")
 			api.Error(rw, http.StatusNotFound, api.Response{Message: err1.Error()})
 			return
 		}
 		if err1 != nil {
+			// fmt.Println("Hello2")
 			api.Success(rw, http.StatusCreated, api.Response{Message: "Enter List"})
 			api.Error(rw, http.StatusInternalServerError, api.Response{Message: err1.Error()})
 			return
@@ -54,28 +60,37 @@ func Login(service Service) http.HandlerFunc {
 		// api.Success(rw, http.StatusOK, resp)
 
 		err := json.NewDecoder(req.Body).Decode(&x)
+		// fmt.Println("Hello00")
 		if err != nil {
+			// fmt.Println("Hello3")
 			api.Error(rw, http.StatusBadRequest, api.Response{Message: err.Error()})
 			return
 		}
 		var jwtString string
 		var isValid int
+		fmt.Println(x.Email, x.Password)
 		for _, v = range resp.User {
-			fmt.Println(x.Email, x.Password)
+			// fmt.Println("Hello4")
+			fmt.Println(v.Email, v.Password)
+			// fmt.Println(v)
 			// fmt.Print(v.Role, v.ID)
-			if v.Email == x.Email && v.Password == x.Password {
-				isValid = 0
-				fmt.Println("Found")
-				jwtString, err = GenerateJWT(v.ID, v.Email, v.Role)
-				break
+			// fmt.Println(CheckPasswordHash(x.Password, v.Password))
+			if v.Email == x.Email {
+				if CheckPasswordHash(x.Password, v.Password) {
+					isValid = 0
+					fmt.Println("Found")
+					jwtString, err = GenerateJWT(v.ID, v.Email, v.Role)
+					break
+				}
 			}
-
 		}
 		if isValid != 0 {
+			// fmt.Println("Hello5")
 			return
 		}
 
 		if err != nil {
+			// fmt.Println("Hello6")
 			api.Error(rw, http.StatusBadRequest, api.Response{Message: err.Error()})
 			return
 		}
@@ -85,35 +100,46 @@ func Login(service Service) http.HandlerFunc {
 
 	})
 }
-
+func CheckPasswordHash(password, hash string) bool {
+	fmt.Println("Hello00")
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	fmt.Println(err)
+	return err == nil
+}
 func Authorize(handler http.HandlerFunc, role int) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		tokenString := r.Header.Get("Authorization")
-		fmt.Println(tokenString)
-		token, _, err := new(jwt.Parser).ParseUnverified(tokenString, jwt.MapClaims{})
-		if err != nil {
-			fmt.Printf("Error %s", err)
+		if tokenString != "" {
+			fmt.Println(tokenString)
+			token, _, err := new(jwt.Parser).ParseUnverified(tokenString, jwt.MapClaims{})
+			if err != nil {
+				fmt.Printf("Error %s", err)
+			}
+
+			if claims, ok := token.Claims.(jwt.MapClaims); ok {
+				fmt.Println(claims)
+				id_auth = fmt.Sprint(claims["id"])
+				email_auth = fmt.Sprint(claims["email"])
+				role_auth = fmt.Sprint(claims["role"])
+
+				fmt.Println("Id = ", id_auth)
+				fmt.Println("Email =", email_auth)
+				fmt.Println("Role =", role_auth)
+
+			} else {
+				api.Success(w, http.StatusCreated, api.Response{Message: "Invalid Token String"})
+			}
+			fmt.Println(values_role[role_auth], role)
+			if values_role[role_auth] > role {
+				fmt.Println("You dont have access to perform this action")
+				return
+			}
+
+			handler.ServeHTTP(w, r)
+		} else {
+			api.Success(w, http.StatusOK, api.Response{Message: "Please Enter token string"})
 		}
-
-		if claims, ok := token.Claims.(jwt.MapClaims); ok {
-			fmt.Println(claims)
-			id_auth = fmt.Sprint(claims["id"])
-			email_auth = fmt.Sprint(claims["email"])
-			role_auth = fmt.Sprint(claims["role"])
-
-			fmt.Println("Id = ", id_auth)
-			fmt.Println("Email =", email_auth)
-			fmt.Println("Role =", role_auth)
-
-		}
-		fmt.Println(values_role[role_auth], role)
-		if values_role[role_auth] > role {
-			fmt.Println("You dont have access to perform this action")
-			return
-		}
-
-		handler.ServeHTTP(w, r)
 	}
 }
 
@@ -135,15 +161,21 @@ func GenerateJWT(ID string, Email string, Role string) (tokenString string, err 
 
 func Create(service Service) http.HandlerFunc {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		var c createRequest
+		var c CreateRequest
 		err := json.NewDecoder(req.Body).Decode(&c)
 		if err != nil {
+			api.Error(rw, http.StatusBadRequest, api.Response{Message: err.Error()})
+			return
+		}
+		err = c.Validate()
+
+		if err != nil {
 			// fmt.Println("error1")
-			api.Error(rw, http.StatusBadRequest, api.Response{Message: "Invalid Datatype Entered"})
+			api.Error(rw, http.StatusBadRequest, api.Response{Message: err.Error()})
 			return
 		}
 
-		err = service.create(req.Context(), c)
+		err = service.Create(req.Context(), c)
 		if isBadRequest(err) {
 			// fmt.Println("error2")
 			api.Error(rw, http.StatusBadRequest, api.Response{Message: err.Error()})
@@ -168,7 +200,26 @@ func isEmailValid(e string) bool {
 func List(service Service) http.HandlerFunc {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		// api.Success(rw, http.StatusCreated, api.Response{Message: "Enter List"})
-		resp, err := service.list(req.Context())
+		resp, err := service.List(req.Context())
+		if err == errNoUsers {
+			// api.Success(rw, http.StatusCreated, api.Response{Message: "Enter List"})
+			api.Error(rw, http.StatusNotFound, api.Response{Message: err.Error()})
+			return
+		}
+		if err != nil {
+			api.Success(rw, http.StatusCreated, api.Response{Message: "Enter List"})
+			api.Error(rw, http.StatusInternalServerError, api.Response{Message: err.Error()})
+			return
+		}
+
+		api.Success(rw, http.StatusOK, resp)
+	})
+}
+
+func Show(service Service) http.HandlerFunc {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		// api.Success(rw, http.StatusCreated, api.Response{Message: "Enter List"})
+		resp, err := service.Show(req.Context())
 		if err == errNoUsers {
 			// api.Success(rw, http.StatusCreated, api.Response{Message: "Enter List"})
 			api.Error(rw, http.StatusNotFound, api.Response{Message: err.Error()})
@@ -188,7 +239,7 @@ func FindByID(service Service) http.HandlerFunc {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		vars := mux.Vars(req)
 
-		resp, err := service.findByID(req.Context(), vars["id"])
+		resp, err := service.FindByID(req.Context(), vars["id"])
 
 		if err == errNoUserId {
 			api.Error(rw, http.StatusNotFound, api.Response{Message: err.Error()})
@@ -207,7 +258,7 @@ func DeleteByID(service Service) http.HandlerFunc {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		vars := mux.Vars(req)
 
-		err := service.deleteByID(req.Context(), vars["id"])
+		err := service.DeleteByID(req.Context(), vars["id"])
 		if err == errNoUserId {
 			api.Error(rw, http.StatusNotFound, api.Response{Message: err.Error()})
 		}
@@ -222,7 +273,7 @@ func DeleteByID(service Service) http.HandlerFunc {
 
 func Update(service Service) http.HandlerFunc {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		var c updateRequest
+		var c UpdateRequest
 
 		err := json.NewDecoder(req.Body).Decode(&c)
 		if err != nil {
@@ -230,7 +281,7 @@ func Update(service Service) http.HandlerFunc {
 			return
 		}
 
-		err = service.update(req.Context(), c)
+		err = service.Update(req.Context(), c)
 		if isBadRequest(err) {
 			api.Error(rw, http.StatusBadRequest, api.Response{Message: err.Error()})
 			return
@@ -245,6 +296,56 @@ func Update(service Service) http.HandlerFunc {
 	})
 }
 
+func UpdatePassword(service Service) http.HandlerFunc {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		var c UpdatePasswordStruct
+		resp, err := service.List(req.Context())
+		if err != nil {
+			api.Error(rw, http.StatusBadRequest, api.Response{Message: err.Error()})
+			return
+		}
+
+		err = json.NewDecoder(req.Body).Decode(&c)
+
+		if err != nil {
+			api.Error(rw, http.StatusBadRequest, api.Response{Message: err.Error()})
+			return
+		}
+		count = 0
+		for _, v = range resp.User {
+			if v.ID == c.ID {
+				fmt.Println(c.Password, v.Password)
+				if CheckPasswordHash(c.Password, v.Password) {
+					count = 1
+					err = service.UpdatePassword(req.Context(), c)
+					if isBadRequest(err) {
+						api.Error(rw, http.StatusBadRequest, api.Response{Message: err.Error()})
+						return
+					}
+
+					if err != nil {
+						api.Error(rw, http.StatusInternalServerError, api.Response{Message: err.Error()})
+						return
+					}
+
+					api.Success(rw, http.StatusOK, api.Response{Message: "Updated Successfully"})
+				}
+			}
+		}
+		if count != 1 {
+			api.Success(rw, http.StatusOK, api.Response{Message: "Invalid Details"})
+		}
+
+	})
+}
+
 func isBadRequest(err error) bool {
 	return err == errEmptyName || err == errEmptyID
 }
+
+/*
+{
+    "duedate":52,
+    "book_id": "501cf224-5009-4d6f-b522-b7b84feeee09",
+    "user_id": "d563e110-ac05-4904-be9c-1cbf42939833"
+}*/
