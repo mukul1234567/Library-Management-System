07931ef872c682c2c6cac3b9f8840da4104e9e14
mukul1234567 <mukulchaudhari01@gmail.com>
2022-10-19 11:25:13 +0530

    Added Validations and additional structs

--- transaction/domain.go
+++ transaction/domain.go
@@ -2,20 +2,20 @@ package transaction
 
 import "github.com/mukul1234567/Library-Management-System/db"
 
-type updateRequest struct {
+type UpdateRequest struct {
 	ID         string `json:"id"`
-	IssueDate  int    `json:"issuedate"`
-	DueDate    int    `json:"duedate"`
-	ReturnDate int    `json:"returndate"`
+	IssueDate  string `json:"issuedate"`
+	DueDate    string `json:"duedate"`
+	ReturnDate string `json:"returndate"`
 	BookID     string `json:"book_id"`
 	UserID     string `json:"user_id"`
 }
 
-type createRequest struct {
+type CreateRequest struct {
 	ID         string `json:"id"`
-	IssueDate  int    `json:"issuedate"`
-	DueDate    int    `json:"duedate"`
-	ReturnDate int    `json:"returndate"`
+	IssueDate  string `json:"issuedate"`
+	DueDate    string `json:"duedate"`
+	ReturnDate string `json:"returndate"`
 	BookID     string `json:"book_id"`
 	UserID     string `json:"user_id"`
 }
@@ -32,7 +32,7 @@ type listResponse struct {
 	Transaction []db.Transaction `json:"transactions"`
 }
 
-func (cr createRequest) Validate() (err error) {
+func (cr CreateRequest) Validate() (err error) {
 	if cr.BookID == "" {
 		return errEmptyBookID
 	}
@@ -42,7 +42,7 @@ func (cr createRequest) Validate() (err error) {
 	return
 }
 
-func (ur updateRequest) Validate() (err error) {
+func (ur UpdateRequest) Validate() (err error) {
 	if ur.BookID == "" {
 		return errEmptyBookID
 	}
